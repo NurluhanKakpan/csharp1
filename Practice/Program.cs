0c9f62055dbc70fabe330fd7f7fcbbb5f59ae355
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-04-13 11:01:40 +0600

    This is task 1

---
+++ Practice/Program.cs
@@ -0,0 +1,14 @@
+﻿using Practice;
+
+public  class Program
+{
+    static  void Main(string[] args)
+    {
+        User user1 = new User("nkakpan",200000);
+        user1.addToBalance(10000);
+        Console.WriteLine(user1.Balance);
+        user1.spendFromBalance(3000000);
+        Console.WriteLine(user1.Balance);
+        
+    }
+}
---
+++ Practice/User.cs
@@ -0,0 +1,62 @@
+﻿namespace Practice;
+
+public class User
+{
+    private string login;
+    private int balance;
+
+    public User(string login, int balance)
+    {
+        this.balance = balance;
+        this.login = login;
+    }
+
+    public void addToBalance(int sum)
+    {
+        balance += sum;
+    }
+
+    public void spendFromBalance( int sum )
+    {
+        if (sum > balance)
+        {
+            Console.WriteLine("You enter sum bigger than your balance");
+            Console.WriteLine("Enter another sum");
+            int summa = Convert.ToInt32(Console.ReadLine());
+            spendFromBalance( summa);
+        }
+        else
+        {
+            balance = balance - sum;
+            Console.WriteLine($"You get from balance {sum} and in your balance have {balance}");
+            
+        }
+        
+    }
+
+    public int Balance
+    {
+        get
+        {
+            return balance;
+        }
+        set
+        {
+            balance = value;
+        }
+    }
+
+    public string Login
+    {
+        get
+        {
+            return login;
+        }
+        set
+        {
+            login = value;
+        }
+    }
+
+    
+}
