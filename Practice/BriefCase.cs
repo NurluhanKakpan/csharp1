fab0a663b9239451abce42c2cf41d2379919d5af
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-04-16 10:42:50 +0600

    This is a task 2

---
+++ Practice/BriefCase.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice;
+
+public class BriefCase
+{
+    
+    private List<Promotion> promotions;
+
+    public BriefCase()
+    {
+        promotions = new List<Promotion>();
+        
+    }
+    
+
+    public List<Promotion> Promotions
+    {
+        get { return promotions; }
+    }
+
+}
